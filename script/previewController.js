55198bd64bb22b7f2ff39a01c494cade25763df0
laedo303 <laedo303@mail.ru>
2022-08-09 18:15:15 +0500

    add-modal

---
+++ script/previewController.js
@@ -0,0 +1,25 @@
+import { form, preview } from './elems.js';
+import { toBase64 } from './utils.js';
+
+const showPreview = (src) => {
+  preview.style.display = 'block';
+  preview.src = src;
+}
+
+export const hidePreview = () => {
+  preview.style.display = '';
+  preview.src = '';
+  form.reset();
+}
+
+export const previewController = () => {
+  hidePreview();
+  const imageFile = form.image;
+
+  image.addEventListener('change', async () => {
+    if (imageFile.files.length) {
+      const src = await toBase64(imageFile.files[0]);
+      showPreview(src);
+    }
+  });
+};
